07d7cfb08584981172d02b7dac652be702359d05
HeRedBo <hhbjkd@163.com>
2017-01-16 13:33:25 +0800

    优化controller.js 代码 修复一点bug

--- web/koa/url2-koa/controller.js
+++ web/koa/url2-koa/controller.js
@@ -10,15 +10,15 @@ function addMapping (router, mapping)
             var path =url.substring(4);
             router.get(path, mapping[url]);
             console.log(`register URL mapping: GET ${path}`);
-        } 
+        }
         else if(url.startsWith('POST '))
         {
             // 如果类型类似 'POST '
             var path = url.substring(5);
             router.post(path, mapping[url]);
             console.log(`register URL mapping : POST ${path}`);
-        } 
-        else 
+        }
+        else
         {
             // 无效的url
             console.log('invalid URL a: ${url}');
@@ -26,25 +26,22 @@ function addMapping (router, mapping)
     };
 }
 
-function addControllers(router)
+function addControllers(router, dir)
 {
-    var files = fs.readdirSync(__dirname + '/controllers');
-    var js_files = files.filter((f) => {
+    fs.readdirSync(__dirname + '/' + dir).filter((f) => {
         return f.endsWith('.js');
-    });
-    for(var f of js_files)
-    {
+    }).forEach((f) => {
         console.log(`process controller: ${f}...`);
         let mapping = require(__dirname + '/controllers/' + f);
         addMapping(router, mapping);
-    };
+    });
 }
 
 module.exports = function(dir)
 {
-    let 
-        controllers_dir = dir || 'controllers';
+    let
+        controllers_dir = dir || 'controllers',
         router = require('koa-router')();
-    addControllers(router , controllers_dir);
+    addControllers(router, controllers_dir);
     return router.routes();
 }
